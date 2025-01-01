a931b3e6dd08fcc2c4f76c6cb0837c3773202237
Jonas I. Liechti <j-i-l@t4d.ch>
2025-01-01 18:32:11 +0100

    allow to set arguments

--- scripts/topviewmouse_example.py
+++ scripts/topviewmouse_example.py
@@ -1,31 +1,39 @@
+import argparse
 import deeplabcut as dlc
 
 
-def main(video_path:str, superanimal_name:str, scale_list) -> None:
+def main(video_path: str, superanimal_name: str, scale_list) -> None:
     """Use ModelZoo to detect poses
 
     Parameters
     ----------
     video_path:
-      Path to the vide ot analyze
+      Path to the video to analyze
     superanimal_name:
-      Identifer of the pretrained model to use
+      Identifier of the pretrained model to use
     scale_list:
-      A collectio of mouse sizes (in # pixels) to test
+      A collection of mouse sizes (in # pixels) to test
     """
 
     dlc.video_inference_superanimal([video_path],
                                     superanimal_name,
                                     scale_list=scale_list,
-                                    video_adapt = False)
+                                    video_adapt=False)
     return None
 
+
 if __name__ == '__main__':
-    video_path = 'demo-video.mp4'
-    superanimal_name = 'superanimal_topviewmouse'
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description='Analyze video using DeepLabCut ModelZoo.')
+    parser.add_argument('video_path', type=str, help='Path to the video to analyze')
+    parser.add_argument('superanimal_name', type=str, help='Identifier of the pretrained model to use')
+    parser.add_argument('--scale_list', type=int, nargs='+', default=list(range(50, 200, 50)),
+                        help='A collection of mouse sizes (in # pixels) to test (default: 50 100 150)')
 
-    scale_list = range(50, 200, 50)
+    # Parse the arguments
+    args = parser.parse_args()
 
-    main(video_path=video_path,
-         superanimal_name=superanimal_name,
-         scale_list=scale_list)
+    # Call the main function with parsed arguments
+    main(video_path=args.video_path,
+         superanimal_name=args.superanimal_name,
+         scale_list=args.scale_list)
